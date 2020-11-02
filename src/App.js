377f74a1454e89fcbd071c4907929d1849865986
lfeliperibeiro <piporibeiro@gmail.com>
2020-11-02 17:46:51 -0300

    feature: require data of API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
+const API = 'https://api.hgbrasil.com/weather?woeid=455827&format=json-cors'
+
 export default class App extends React.Component {
+  state = {
+    city: ''
+  }
+
+  componentDidMount() {
+    fetch(API) 
+    .then(response => response.json())
+    .then(json => {
+        this.setState({
+        city: json.results.city_name
+      })
+    })
+  }
+
   render() {
     return (
-      <h1>Helo world</h1>
+      <div>
+        <h1>{this.state.city}</h1>
+      </div>
     )
   }
 }
